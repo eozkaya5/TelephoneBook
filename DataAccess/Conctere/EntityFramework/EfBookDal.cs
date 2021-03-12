a04460e989288a662088ce0820f6f2a9cf037425
eozkaya5 <eozkaya675@gmail.com>
2021-03-12 03:15:01 +0300

    Dallar yazıldı.

---
+++ DataAccess/Conctere/EntityFramework/EfBookDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess;
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstarct;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Conctere.EntityFramewok
+{
+    public class EfBookDal:EfEntityRepositoryBase<Book,BookDbContext>,IBookDal
+    {
+    }
+}
