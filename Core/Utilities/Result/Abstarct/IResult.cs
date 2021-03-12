da291f64f129f775f128317953be5554116fcf07
eozkaya5 <eozkaya675@gmail.com>
2021-03-12 03:34:51 +0300

    Result yazıldı.

---
+++ Core/Utilities/Result/Abstarct/IResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Result.Abstarct
+{
+    public interface IResult
+    {
+        bool Success { get; }//sadece okunabilir(set; yaz demek)
+        string Message { get; }
+    }
+}
