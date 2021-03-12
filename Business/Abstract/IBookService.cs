8290b2d2105e4aa5fa38a177aeead5fa17dea545
eozkaya5 <eozkaya675@gmail.com>
2021-03-12 03:33:57 +0300

    Methodlar yazıldı.

---
+++ Business/Abstract/IBookService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Result.Abstarct;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IBookService
+    {
+        IResult Add(Book book);
+        IResult Update(Book book);
+        IResult Delete(Book book);
+        IDataResult<List<Book>> GetAll(int id);
+
+    }
+}
