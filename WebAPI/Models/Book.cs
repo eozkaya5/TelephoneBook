14949df540a6f5283b3bf463daeb07661fad38d7
eozkaya5 <eozkaya675@gmail.com>
2021-03-13 02:36:22 +0300

    Edited.

--- WebAPI/Models/Book.cs
+++ WebAPI/Models/Book.cs
@@ -4,14 +4,15 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities
+namespace WebAPI.Models
 {
-    public class Book :IEntity
+    public class Book
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string SurName { get; set; }
         public int TelephoneNumber { get; set; }
-        
+        public int UserId { get; set; }
+
     }
 }
