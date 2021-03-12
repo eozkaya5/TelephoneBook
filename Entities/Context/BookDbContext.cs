0e3e60749ea198434d72c3e39e19fedc6fbcea09
eozkaya5 <eozkaya675@gmail.com>
2021-03-12 05:00:59 +0300

    Klosör değiştirildi.

---
+++ Entities/Context/BookDbContext.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities.Concrete;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities.Context
+{
+    public class BookDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TelephoneBook;Trusted_Connection=true");
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    }
+}
