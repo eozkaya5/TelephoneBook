8471a4c83031d24b302f2e4278762b3119878fa8
eozkaya5 <eozkaya675@gmail.com>
2021-03-12 04:56:42 +0300

    Methodlar eklendi.

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -6,7 +6,8 @@ using System.Text;
 namespace Business.Abstract
 {
     public interface IUserService
-    {       
+    {     
+        List<OperationClaim> GetClaims(User user);
         void Add(User user);
         User GetByMail(string email);
     }
