2fa49e9906f7405b3203ba10e763fd989fcceebc
eozkaya5 <eozkaya675@gmail.com>
2021-03-12 21:16:22 +0300

    user eklendi.

--- WebAPI/Models/Authentication/AppRole.cs
+++ WebAPI/Models/Authentication/AppRole.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Core.Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace WebAPI.Models.Authentication
 {
-    public class AppRole:IdentityRole
+    public class AppRole:IdentityRole<User>
     {
     }
 }
