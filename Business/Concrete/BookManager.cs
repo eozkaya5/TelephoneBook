8f162642afa60dd801a440fcd8297f8dbe3b82b3
eozkaya5 <eozkaya675@gmail.com>
2021-03-12 03:34:15 +0300

    İş kodları yazıldı.

---
+++ Business/Concrete/BookManager.cs
@@ -0,0 +1,45 @@
+﻿using Business.Abstract;
+using Business.Contants;
+using Core.Utilities.Result.Abstarct;
+using Core.Utilities.Result.Concrete;
+using DataAccess.Abstarct;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class BookManager : IBookService
+    {
+        IBookDal _bookDal;
+
+        public BookManager(IBookDal bookDal)
+        {
+            _bookDal = bookDal;
+        }
+
+        public IResult Add(Book book)
+        {
+            _bookDal.Add(book);
+            return new SuccessResult(Messages.Added);
+        }
+
+        public IResult Delete(Book book)
+        {
+            _bookDal.Delete(book);
+            return new SuccessResult(Messages.Deleted);
+        }
+
+        public IDataResult<List<Book>> GetAll(int id)
+        {
+           return new SuccessDataResult<List<Book>> (_bookDal.GetAll(x=>x.Id==id),Messages.Listed);
+        }
+
+        public IResult Update(Book book)
+        {
+            _bookDal.Update(book);
+            return new SuccessResult(Messages.Updated);
+        }
+    }
+}
---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using Core.Entities.Concrete;
+using DataAccess.Abstract;
+using System.Collections.Generic;
+
+namespace Business.ConCrete
+{
+    public class UserManager : IUserService
+    {
+        IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }      
+        public void Add(User user)
+        {
+            _userDal.Add(user);
+        }
+
+        public User GetByMail(string email)
+        {
+            return _userDal.Get(u => u.Email == email);
+        }
+    }
+}
