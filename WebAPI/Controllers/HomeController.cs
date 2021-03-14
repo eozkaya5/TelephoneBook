44b18c0d24720416eaa286de01c7b5f6dfdd9554
eozkaya5 <eozkaya675@gmail.com>
2021-03-14 12:25:53 +0300

    Cache oluşturma kodları yazıldı.

--- WebAPI/Controllers/HomeController.cs
+++ WebAPI/Controllers/HomeController.cs
@@ -1,45 +1,69 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
+using NLog;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WebAPI.Models;
+using LogLevel = NLog.LogLevel;
 
 namespace WebAPI.Controllers
 {
+   
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public ILogger<HomeController> _logger;
+        private IMemoryCache memoryCache;
+        public HomeController(ILogger<HomeController> logger, IMemoryCache cache)
         {
             _logger = logger;
+           this.memoryCache = cache;
+
         }
-   
-        public IActionResult Information()
-        {
-            _logger.LogInformation("Information mesajı...");
-            return View();
-        }
-        public IActionResult Error()
-        {
-            _logger.LogError("Error mesajı...");
-            return View();
-        }
-        public IActionResult Warning()
-        {
-            _logger.LogWarning("Warning mesajı...");
-            return View();
-        }
+        //public IActionResult Information()
+        //{
+        //    _logger.LogInformation("Information mesajı...");
+        //    return View();
+        //}
+        //public IActionResult Error()
+        //{
+        //    _logger.LogError("Error mesajı...");
+        //    return View();
+        //}
+        //public IActionResult Warning()
+        //{
+        //    _logger.LogWarning("Warning mesajı...");
+        //    return View();
+        //}
+       [Route("/Index")]
         public IActionResult Index()
         {
             ViewBag.UserName = User.Identity.Name;
+            DateTime time;
 
+            bool cacheEntryOptios = memoryCache.TryGetValue("Time", out time);
+            if (!cacheEntryOptios)
+            {
+                time = DateTime.Now;
+                var cache = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromSeconds(30));
+                object p = memoryCache.Set("Time", time, cache);
+            }
+                  
+            return View(time);
+        }
+        [Route("/Error")]
+        public IActionResult Error()
+        {
+            var error = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            var logger = LogManager.GetLogger("FileManager");
+            logger.Log(LogLevel.Error, $"\nHatanın gerçekleştiği yer:{error.Path} \nHata: {error.Error.Message}\nStackTrace:{ error.Error.StackTrace}");
             return View();
         }
 
-       
     }
 }
