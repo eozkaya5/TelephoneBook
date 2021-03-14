137f2f491c8d8ce3d2d46b29bf0578bcc7e6994c
eozkaya5 <eozkaya675@gmail.com>
2021-03-14 15:07:58 +0300

    Hata mesajı işlemi yazıldı.

--- WebAPI/Controllers/HomeController.cs
+++ WebAPI/Controllers/HomeController.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WebAPI.Models;
+using LogLevel = NLog.LogLevel;
 
 namespace WebAPI.Controllers
 {
@@ -23,11 +26,7 @@ namespace WebAPI.Controllers
             _logger.LogInformation("Information mesajı...");
             return View();
         }
-        public IActionResult Error()
-        {
-            _logger.LogError("Error mesajı...");
-            return View();
-        }
+     
         public IActionResult Warning()
         {
             _logger.LogWarning("Warning mesajı...");
@@ -40,6 +39,13 @@ namespace WebAPI.Controllers
             return View();
         }
 
-       
+        [Route("/Error")]
+        public IActionResult Error()
+        {
+            var error = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            var logger = LogManager.GetLogger("FileManager");
+            logger.Log(LogLevel.Error, $"\nHatanın gerçekleştiği yer:{error.Path} \nHata: {error.Error.Message}\nStackTrace:{ error.Error.StackTrace}");
+            return View();
+        }
     }
 }
